10a899c52a3bd35b94d2904b855c834f49f554d7
1059752368 <1059752368@qq.com>
2018-06-04 12:43:31 +0800

    second

---
+++ src/main/java/com/how2java/tmall/controller/PropertyController.java
@@ -0,0 +1,69 @@
+package com.how2java.tmall.controller;
+
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.Property;
+import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+public class PropertyController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    PropertyService propertyService;
+
+    @RequestMapping("admin_property_add")
+    public String add(Model model, Property property){
+        propertyService.add(property);
+        return "redirect:admin_property_list?cid="+property.getCid();
+    }
+
+    @RequestMapping("admin_property_delete")
+    public String delete(int id){
+        Property p = propertyService.get(id);
+        propertyService.delete(id);
+        return "redirect:admin_property_list?cid="+p.getCid();
+    }
+
+    @RequestMapping("admin_property_edit")
+    public String edit(Model model, int id) {
+        Property property = propertyService.get(id);
+        Category category = categoryService.get(property.getCid());
+        property.setCategory(category);
+        model.addAttribute("p", property);
+        return "admin/editProperty";
+    }
+
+    @RequestMapping("admin_property_update")
+    public String update(Property property){
+        propertyService.update(property);
+        return "redirect:admin_property_list?cid="+property.getCid();
+    }
+
+    @RequestMapping()
+    public String list(int cid, Model model, com.how2java.tmall.util.Page page){
+        Category category = categoryService.get(cid);
+        PageHelper.offsetPage(page.getStart(),page.getCount());
+        List<Property> ps = propertyService.list(cid);
+
+        int total = (int) new PageInfo<>(ps).getTotal();
+        page.setTotal(total);
+        page.setParam("&cid="+category.getId());
+
+        model.addAttribute("ps", ps);
+        model.addAttribute("category", category);
+        model.addAttribute("page", page);
+        return "admin/listProperty";
+    }
+}
